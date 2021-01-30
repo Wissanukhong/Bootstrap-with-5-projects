2a7af06389f4e0cabd31d6417ad42dbd72e86507
Wissanu.khong <wissanu.khong@gmail.com>
2021-01-30 17:16:27 +0700

    refactoring project

--- assets/js/myscript.js
+++
@@ -1,9 +0,0 @@
-// navbar scroll
-$(window).scroll(function() {
-    var scrollTop = $(this).scrollTop();
-    if (scrollTop > 1) {
-        $('#navbar').css('padding', '0 20px')
-    } else {
-        $('#navbar').css('padding', '20px')
-    }
-})
